43813b9d732feb4dafec8b0fc1261bc2cf3d445e
hamed <blueNoteBook@DESKTOP-2OV0I4P>
2022-11-16 18:24:38 +0330

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class test
+{
+    public static void Main()
+    { 
+        Console.WriteLine("enter number:");
+        string a = Console.ReadLine();
+        int x = Convert.ToInt32(a);
+        Console.WriteLine(tavan_2(x));
+        Console.WriteLine(tavan_3(x));
+    }
+    private static double tavan_2 (int x)
+    {
+        double y = Math.Pow(x,2);
+        double z = Math.Pow(x, 3);
+        return (y);
+    }
+    private static double tavan_3(int x)
+    {
+        double y = Math.Pow(x,3);
+        return (y);
+    }
+}
